6edde04caccc7380467008a1d26bd25bb5541487
Vincent <vincentnewkirk@gmail.com>
2016-11-03 11:03:01 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import { Router, Route, Link } from 'react-router';
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
